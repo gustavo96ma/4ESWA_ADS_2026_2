67c6b0f44e1285736d9ba6cccfac7f1935d59dfd
Gustavo <gustavo96ma@gmail.com>
2026-08-27 10:51:52 -0300

    20-08

---
+++ 20-08/hello_world_again/README.md
@@ -0,0 +1,3 @@
+# hello_world_again
+
+A new Flutter project.
---
+++ 20-08/hello_world_again/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Text('Hello World!'),
+              TextButton(onPressed: () {}, child: Text('BUTÃO')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
